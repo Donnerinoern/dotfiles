e53e5425b8d05e57df3cdf1343d723cd35f77f33
donnan <sondresalvestad@gmail.com>
2024-01-03 00:23:26 +0100

    nvim-cmp -> coq. Also updated flake.lock

--- modules/gui/nixvim/default.nix
+++ modules/gui/nixvim/default.nix
@@ -58,6 +58,7 @@
 
     colorschemes.gruvbox.enable = true;
     luaLoader.enable = true;
+
     plugins = {
       comment-nvim.enable = true;
       alpha = {
@@ -72,21 +73,60 @@
         };
         keymap.openFileFolder.tertiary = ["<leader>t" "middlemouse"];
       };
-      # coq-nvim = {
-      #   enable = true;
-      #   installArtifacts = true;
-      #   recommendedKeymaps = true;
-      #   alwaysComplete = true;
-      #   autoStart = true;
-      # };
-      nvim-cmp = {
+      coq-nvim = {
         enable = true;
-        completion = {
-          autocomplete = [
-            "TextChanged"
-          ];
-        };
+        installArtifacts = true;
+        recommendedKeymaps = true;
+        alwaysComplete = true;
+        autoStart = true;
       };
+      # nvim-cmp = {
+      #   enable = true;
+      #   # autoEnableSources = true;
+      #   sources = [
+      #     # { name = "ultisnips"; }
+      #     { name = "luasnip"; }
+      #     { name = "path"; }
+      #     { name = "buffer"; }
+      #     { name = "nvim_lsp"; }
+      #   ];
+      #   # completion = {
+      #   #   autocomplete = [
+      #   #     "TextChanged"
+      #   #   ];
+      #   #   keywordLength = 1;
+      #   #   keywordPattern = "\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)";
+      #   # };
+      #   snippet.expand = "luasnip";
+      #   mapping = {
+      #     "<CR>" = "cmp.mapping.confirm({ select = true })";
+      #     "<Tab>" = {
+      #       action = ''
+      #         function(fallback)
+      #           if cmp.visible() then
+      #             cmp.select_next_item()
+      #           elseif luasnip.expendable() then
+      #             luasnip.expand()
+      #           elseif luasnip.expand_or_jumpable() then
+      #             luasnip.expand_or_jump()
+      #           elseif check_backspace() then
+      #             fallback()
+      #           else
+      #             fallback()
+      #           end
+      #         end
+      #       '';
+      #     };
+      #   };
+      # };
+      #
+      # luasnip.enable = true;
+
+      # cmp-nvim-ultisnips.enable = true;
+      # cmp-buffer.enable = true;
+      # cmp-path.enable = true;
+      # cmp-nvim-lsp.enable = true;
+      # cmp_luasnip.enable = true;
       
       telescope = {
         enable = true;
@@ -128,6 +168,10 @@
       leap.enable = true;
       which-key.enable = true;
       fidget.enable = true;
+      markdown-preview = {
+        enable = true;
+        autoStart = true;
+      };
       treesitter = {
         enable = true;
         indent = true;
